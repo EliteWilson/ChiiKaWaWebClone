cd9f04574a085451b1599fecc8a1187dee827cc4
EliteWilson <elitewilson69420@gmail.com>
2025-03-08 18:47:29 +0800

    betaNavInner

---
+++ JS/Other.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("delivery").addEventListener("click", function() {
+        alert("國際配送功能尚未開放");
+    });
+    document.getElementById("chatbot").addEventListener("click", function() {
+        alert("國際配送功能尚未開放");
+    });
+});
