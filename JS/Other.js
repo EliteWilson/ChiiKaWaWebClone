3c9226af593ec9c56dca9ca0c915293114b12a77
EliteWilson <elitewilson69420@gmail.com>
2025-03-09 09:47:49 +0800

    0309BeforeTest

--- JS/Other.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("delivery").addEventListener("click", function() {
-        alert("國際配送功能尚未開放");
-    });
-    document.getElementById("chatbot").addEventListener("click", function() {
-        alert("國際配送功能尚未開放");
-    });
-});
